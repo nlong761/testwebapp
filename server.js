969b3dd20d9b63068264c092a40dcc8ee2663d29
Nam Nguyen Khac <Nam Nguyen Khac>
2017-09-09 11:49:33 +0700

    add project

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var Beer = require('./models/beer');
+var app = express();
+var port = process.env.PORT || 3000;
+var router = express.Router();
+mongoose.connect('mongodb://localhost:27017/test');
+router.get('/', function(req, res) {
+    res.json({ message: 'You are running dangerously low on beer!' });
+});
+app.use('/api', router);
+app.listen(port);
+console.log('Insert beer on port ' + port);
