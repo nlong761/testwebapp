3ba8d2e6a1e81c5f113b10565c8236de0c3a1f11
namLIVE <nlong761@gmail.com>
2017-10-20 00:58:38 +0700

    add test

--- test/test3.js
+++ test/test3.js
@@ -1,9 +1,28 @@
-var http = require("http");
-http.get({
-    hostname: 'localhost',
-    port: 8081,
-    path: '/',
-    agent: false  // create a new agent just for this one request
-  }, (res) => {
-    console.log(res.rawHeaders);
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/myappdatabase');
+
+var User = require('./models/user');
+
+// create a new user called chris
+var chris = new User({
+  name: 'Chris',
+  username: 'sevilayha2',
+  password: 'password' 
+});
+
+// call the custom method. this will just add -dude to his name
+// user will now be Chris-dude
+chris.dudify(function(err, name) {
+  if (err) throw err;
+  console.log('Your new name is ' + name);
+});
+
+// call the built-in save method to save to the database
+User.find({name: chris.name}, function(err, ...users){
+  
+});
+chris.save(function(err) {
+  if (err) throw err;
+
+  console.log('User saved successfully!');
 });
