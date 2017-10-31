4ed238cc957cfe30d095d515ea665238497a573a
nam <nkn6294@live.co.uk>
2017-10-31 19:15:12 +0700

    modify

--- test/test2.js
+++ test/test2.js
@@ -1,9 +1,28 @@
-var http = require("http");
+var util = require('util');
+var EventEmitter = require('events').EventEmitter;
 
-http.createServer(function (req, res) {
-    
-    console.log(req.headers);
-    res.end(JSON.stringify(req.headers));
-}).listen(8081, function() {
-    console.log('Server running at http://127.0.0.1:8081/');
+function Counter() {
+    var self = this;
+    EventEmitter.call(this);//call EventEmitter constructor
+    var count = 0;
+
+    this.start = function() {
+        this.emit('start');
+        setInterval(function() {
+            self.emit('count', count);
+            ++count;
+        }, 1000);
+    }
+}
+util.inherits(Counter, EventEmitter);//setup inheritance
+
+var counter = new Counter();
+
+counter.on('start', function() {
+    console.log('start event');
 });
+counter.on('count', function(count) {
+    console.log('count = ' + count);
+});
+
+counter.start();
