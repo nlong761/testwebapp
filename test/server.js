eaf57c6603263d1ab1286195ccc0407aa3aa95b9
nam <nkn6294@live.co.uk>
2017-11-19 00:00:10 +0700

    modify

--- test/server.js
+++ test/server.js
@@ -1,168 +1,42 @@
-var express = require('express');
-var app = express();
-var fs = require("fs");
-
-var bodyParser = require('body-parser');
-var multer = require('multer');
-
-app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(multer({ dest: '/tmp/' }));
-var storage = multer.memoryStorage();
-// storage = multer.diskStorage({
-//     destination: function(req, file, cb) {
-//         cb(null, 'uploads/');
-//     },
-//     filename: function(req, file, cb) {
-//         cb(null, file.fieldname + '-' + Date.now());
-//     }
-// });
-var upload = multer({
-    dest: 'uploads/',
-    storage: storage,
-    limits: {
-        fileSize: 1 << 22, // 4M 
-    },
+var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/test2', {
+    useMongoClient: true,
+}).then(() => {
+    console.log("Connection db success!");
+}).catch(error => {
+    console.log("Connection db error:" + error);
 });
-var file_upload = upload.single('file_upload');
-app.post('/files/upload', file_upload, function (req, res, next) {
-    res.send({textfields: req.body, file:req.file});
-}, function(err, req, res, next) {
-    res.send({err: err});
+var Schema = mongoose.Schema;
+//---------------------SCHEMA---------------------
+var userSchema = new Schema({
+    _id: Number,
+    name: String,
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    // admin: Boolean,
+    // location: String,
+    // meta: {
+    //     age: Number,
+    //     website: String
+    // },
+    // created_at: Date,
+    // updated_at: Date
 });
-
-// app.get('/index.htm', function (req, res) {
-//     res.sendFile(__dirname + "/" + "index.htm");
-// })
-
-// app.post('/file', function (req, res) {
-//     console.log(req.files.file.name);
-//     console.log(req.files.file.path);
-//     console.log(req.files.file.type);
-//     var file = __dirname + "/" + req.files.file.name;
-//     fs.readFile(req.files.file.path, function (err, data) {
-//         fs.writeFile(file, data, function (err) {
-//             if (err) {
-//                 console.log(err);
-//             } else {
-//                 response = {
-//                     message: 'File uploaded successfully',
-//                     filename: req.files.file.name
-//                 };
-//             }
-//             console.log(response);
-//             res.end(JSON.stringify(response));
-//         });
+var User = mongoose.model('User', userSchema);
+//------------------------------------------------
+// for (let i = 0; i < 10; i++) {
+//     let user = new User({
+//         _id: i,
+//         username: 'name_' + i,
+//         password: 'password_' + i,
 //     });
-// })
-
-var server = app.listen(3000, function () {
-    var host = server.address().address
-    var port = server.address().port
-
-    console.log("Example app listening at http://%s:%s", host, port)
-})
-
-
-//____________________________________
-// var express = require("express");
-// var app = express();
-
-// app.use(express.static("public"));
-
-// app.get('/', function(req, res) {
-//     res.send("Hello word");
-// });
-
-// app.get('image', function(req, res) {
-
-// });
-
-// var server = app.listen(3000, function(req, res) {
-//     var host = server.address().address;
-//     var port = server.address().port;
-
-//     console.log("Running at: http://" + host + ":" + port);
-// });
-//__________________________________
-// var promise1 = new Promise((resolve, reject) => {
-
-// });
-
-// var buf = new Buffer('abc');
-// var json = buf.toJSON();
-// console.log(json);
-
-//--------------------------------------
-// var buf = new Buffer(26);
-// for (var i = 0; i < 26; i++) {
-//     buf[i] = i + 97;
-// }
-// console.log(buf.toString('ascii'));
-
-//--------------------------------
-// var events = require('events');
-// var eventEmitter = new events.EventEmitter();
-
-// var listner1 = function listner1() {
-//     console.log('listner1 executed.');
-// };
-
-// var listner2 = function listner2(message) {
-//     console.log('listner2 excuted:' + message);
-// };
-
-// eventEmitter.addListener('connection', listner1);
-// eventEmitter.on('connection', listner2);
-
-// var eventListeners = require('events').EventEmitter.listenerCount(eventEmitter, 'connection');
-// console.log(eventListeners + " Listner(s) listening to connection event");
-// eventEmitter.emit('connection');
-// eventEmitter.removeListener('connection', listner1);
-// console.log("Listner1 will not listen now.");
-// eventEmitter.emit('connection', "test");
-// eventListeners = require('events').EventEmitter.listenerCount(eventEmitter,'connection');
-// console.log(eventListeners + " Listner(s) listening to connection event");
-
-// console.log("Program Ended.");
-
-//-----------------------------------------------------
-// var events = require('events');
-// var eventEmitter = new events.EventEmitter();
-
-// var connectHandler = function connected() {
-//     console.log('connection successful.');
-//     eventEmitter.emit('data_received');
+//     user.save();
 // }
 
-// eventEmitter.on('connection', connectHandler);
+User.findById(1).then(user => console.log(user));
 
-// eventEmitter.on('data_received', function () {
-//     console.log('data received succesfully.');
-// });
 
-// eventEmitter.emit('connection');
-// console.log("Program Ended");
-//-----------------------------------------------------------------
-// const https = require('https');
-// https.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', (resp)=>{
-//     let data = '';
-//     resp.on('data', (chunk) => {
-//         data += chunk;
-//     });
-//     resp.on('end', () => {
-//         console.log(JSON.parse(data).explanation);
-//     });
-// }).on('error', (err) => {
-//     console.log("Error:" + err.message);
-// });
-
-//-----------------------------------------------
-// var fs = require("fs");
-// fs.readFile("note.txt", function(err, data){
-//     if (err) {
-//         return console.error(err);
-//     }
-//     console.log(data.toString());
-// });
-// console.log("Program Ended");
+mongoose.disconnect(function (err) {
+    return console.error("Error:" + err);
+});
